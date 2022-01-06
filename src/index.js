2ddad6f674827cdb3e84387437fb125b44d1bddf
alterego3003 <pav.sudakov@live.ru>
2022-01-06 21:30:44 +0400

    init: add solution at index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 module.exports = function toReadable (number) {
-    const digits=['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const tens=['','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const digits=[
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    const tens=[
+        '','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
     let numberDigits = Array.from(number.toString());
-    if (number == 0) {
+    if (number === 0) {
         return 'zero';
     }
     else if (number <= 19) {
@@ -10,7 +14,7 @@ module.exports = function toReadable (number) {
     }
     else if (number <= 99) {
         let result = `${tens[numberDigits[0] - 1]} ${digits[numberDigits[1]]}`; 
-        return result.trim();; 
+        return result.trim(); 
     }
     else {
         let decimals = `${numberDigits[1]}${numberDigits[2]}`;
@@ -27,7 +31,5 @@ module.exports = function toReadable (number) {
         }
   
     }
-  
-    
   
   };
